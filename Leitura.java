2309f8016d804c50ffe1f98c41741f19d12e6fe0
Renan Martins Conceição Silva de Lima <92929174+Renanmali@users.noreply.github.com>
2023-04-19 17:37:27 -0300

    Create Leitura.java
    Programa que faz a leitura de um número e determina a quantidade de dígitos

---
+++ Leitura.java
@@ -0,0 +1,21 @@
+// Determine o	número	de	dígitos	de	um	número	informado
+
+import java.util.Scanner;
+public class Leitura{
+    public static void main(String[] args){
+        try(Scanner teclado = new Scanner(System.in)){
+            int i=0;
+            System.out.println("Escreva o número que deseja: ");    
+            String numero = teclado.nextLine();
+            for (char número: numero.toCharArray()) {
+                if(número == '.' || número == ' ' || número == ','){
+                    i = i;
+                }
+                else{
+                    i++;
+                }
+            }
+            System.out.println("Este número possui: " + i + " dígitos");
+        }
+    }
+}
